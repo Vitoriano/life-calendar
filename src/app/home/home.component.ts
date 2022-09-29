9c0a9ca8d1bb92052971c7724febca431926720b
Vitoriano <vitorianoernandes2012@gmail.com>
2022-09-28 23:27:10 -0300

    Adicionando cores nas box dinamicamente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,22 +10,42 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   public matrizValues: any[] = [];
+  public qtdSemanas: Number = 52;
+  public yearsOfLife: Number = 90;
 
   ngOnInit(): void {
 
-    const Qtdsemanas: Number = 52;
-    const life: Number = 90;
     const LifeMatriz: any[] = [];
-
-    for(let i = 1; i < Qtdsemanas; i++) {
+    for(let i = 1; i < this.qtdSemanas; i++) {
       LifeMatriz[i] = [];
-      for(let j = 1; j <= life; j++) {
-        LifeMatriz[i].push(j);
+      for(let j = 1; j <= this.yearsOfLife; j++) {
+        LifeMatriz[i].push({class: 'isGreen'});
       }
 
     }
     this.matrizValues = LifeMatriz;
     console.log(LifeMatriz);
+    //this.calculateYears();
+  }
+
+
+  calculateYears() {
+    let idade: any = 25;
+    let result: any = Number(this.qtdSemanas) * idade;
+    let qtdSemana: any = result / Number(this.yearsOfLife);
+    //console.log(result);
+
+   this.matrizValues.forEach((item, index) => {
+
+    if(index <= 25) {
+      item.map((data: any) => {
+        data.class = "isRed";
+      })
+    }
+
+
+   });
+
   }
 
 }
